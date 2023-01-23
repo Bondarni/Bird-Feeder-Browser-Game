35fcfd82987e255b79e5a5e9e5f69b40c1a8e82a
Bondarni <brandon.partrick@gmail.com>
2023-01-23 18:40:43 -0500

    Added Timer Mechanic

--- script.js
+++ script.js
@@ -6,16 +6,40 @@ const grains = document.querySelector('#Grain')
 const berries = document.querySelector('#Berry')
 const plates = document.querySelectorAll('.plate')
 const birds = document.querySelectorAll('.customer')
-const time = document.querySelector('#timer')
+const openSign = document.querySelector('#start-game')
+const restaurant = document.querySelector('#game-area')
 let handsEmpty = true
+
+function countdown() {
+  console.log('Now Open!')
+  let time = 61
+  let countdown = setInterval(function () {
+    time--
+    document.querySelector('#clock').innerHTML = '00:' + time
+    if (time === 0) {
+      clearInterval(countdown)
+      console.log("We're Closed!")
+      console.log("You've raked up " + score + ' points today.')
+    }
+  }, 1000)
+}
+
+const chooseOrder = () => {
+  math.floor(math.random() * max)
+}
 
 let emptyHands = () => {
   handsEmpty = true
 }
+
+let takeOrder = (event) => {
+  console.log(`I'm Hungry!`)
+}
+
 let grabFood = (event) => {
   dishName = event.target.innerText
   handsEmpty = false
-  console.log("You're holding a " + dishName + '!')
+  console.log(`You're holding a ` + dishName + `.`)
 }
 let orderUp = () => {
   if (handsEmpty === false) {
@@ -29,15 +53,22 @@ let orderUp = () => {
     console.log("Uh...Where's my food?")
   }
 }
+let openService = (event) => {
+  countdown()
+}
 
 // seeds.addEventListener('click', grabFood)
 // grains.addEventListener('click', grabFood)
 // berries.addEventListener('click', grabFood)
 
+openSign.addEventListener('click', countdown)
 foods.forEach(function (food) {
   food.addEventListener('click', grabFood)
 })
 plates.forEach(function (plate) {
   plate.addEventListener('click', orderUp)
+})
+birds.forEach(function (bird) {
+  bird.addEventListener('click', takeOrder)
 })
 //remember ^this^ for event listeners on classes.
